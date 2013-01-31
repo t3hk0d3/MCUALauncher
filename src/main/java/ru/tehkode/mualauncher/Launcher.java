53ef191605f0d7fc9bfecc079a0229626387ff33
Igor Yamolov <clouster@yandex.ru>
2013-01-31 09:15:44 +0400

    Fixed startup sequence

--- src/main/java/ru/tehkode/mualauncher/Launcher.java
+++ src/main/java/ru/tehkode/mualauncher/Launcher.java
@@ -7,6 +7,7 @@ import java.io.File;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import ru.tehkode.mualauncher.updater.LauncherVersion;
 import ru.tehkode.mualauncher.utils.Logger;
 import ru.tehkode.mualauncher.utils.PlatformUtils;
 import ru.tehkode.mualauncher.utils.Resources;
@@ -14,13 +15,26 @@ import ru.tehkode.mualauncher.utils.Resources;
 public class Launcher {
 
     public static void main(String[] args) throws Exception {
+        System.setProperty("http.agent", "MUALauncher/1.0");
+
+        final File currentPath = PlatformUtils.getApplicationPath("mualauncher");
+        final LauncherOptions options = new LauncherOptions(new File(currentPath, "launcher.options"));
+
+        // initalize logger output        
+        Logger.initialize(new File(currentPath, "launcher.log"));
+
+        Resources.initialize(options);
+
+        Logger.info("Loading MUALauncher version '%s'", LauncherVersion.getLocalVersion().toString());
+
+
         GraphicsEnvironment ge =
                 GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice gd = ge.getDefaultScreenDevice();
 
         //If shaped windows aren't supported, exit.
         if (!gd.isWindowTranslucencySupported(GraphicsDevice.WindowTranslucency.PERPIXEL_TRANSPARENT)) {
-            System.err.println("Shaped windows are not supported");
+            Logger.error("Shaped windows are not supported");
         }
 
         try {
@@ -30,15 +44,15 @@ public class Launcher {
             //
         }
 
-        System.setProperty("http.agent", "MUALauncher/1.0");
-
-        final File currentPath = PlatformUtils.getApplicationPath("mualauncher");
-        final LauncherOptions options = new LauncherOptions(new File(currentPath, "launcher.options"));
-
-        Resources.initialize(options);
-
-        // initalize logger output        
-        Logger.initialize(new File(currentPath, "launcher.log"));
+        // check new version
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                } catch (Throwable e) {
+                }
+            }
+        });
 
         // Create the GUI on the event-dispatching thread
         SwingUtilities.invokeLater(new Runnable() {
