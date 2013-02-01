6cb1eab4b3115861ff36376493fc4a71408f5614
Igor Yamolov <clouster@yandex.ru>
2013-02-01 17:31:52 +0400

    Imporved launcher version

--- src/main/java/ru/tehkode/mualauncher/updater/LauncherVersion.java
+++ src/main/java/ru/tehkode/mualauncher/updater/LauncherVersion.java
@@ -1,8 +1,9 @@
 package ru.tehkode.mualauncher.updater;
 
 import com.google.gson.Gson;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URL;
+import org.apache.commons.io.IOUtils;
 import ru.tehkode.mualauncher.utils.Logger;
 
 /**
@@ -50,22 +51,58 @@ public class LauncherVersion implements Comparable<LauncherVersion> {
         return String.format("%s (build: %d)", version, build);
     }
 
-    private static LauncherVersion readLocalVersion() {
-        try {
-            Gson gson = new Gson();
-
-            InputStreamReader reader = new InputStreamReader(LauncherVersion.class.getResourceAsStream("/version.json"));
-
-            return gson.fromJson(reader, LauncherVersion.class);
-        } catch (Throwable e) {
-            Logger.error("Failed to read local version: (%s) %s ", e.getClass().getSimpleName(), e.getMessage());
-            
-            return new LauncherVersion("unknown", 0, null);
-        }
-    }
-
     @Override
     public int compareTo(LauncherVersion o) {
         return this.getBuild() - o.getBuild();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + (this.version != null ? this.version.hashCode() : 0);
+        hash = 31 * hash + this.build;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LauncherVersion other = (LauncherVersion) obj;
+        if ((this.version == null) ? (other.version != null) : !this.version.equals(other.version)) {
+            return false;
+        }
+        if (this.build != other.build) {
+            return false;
+        }
+        return true;
+    }
+
+    public String toJson() {
+        Gson gson = new Gson();
+
+        return gson.toJson(this);
+    }
+
+    public static LauncherVersion fromJson(String json) {
+        Gson gson = new Gson();
+
+        return gson.fromJson(json, LauncherVersion.class);
+    }
+
+    private static LauncherVersion readLocalVersion() {
+        try {
+            String localVersion = IOUtils.toString(LauncherVersion.class.getResourceAsStream("/version.json"));
+
+            return fromJson(localVersion);
+        } catch (Throwable e) {
+            Logger.error("Failed to read local version: (%s) %s ", e.getClass().getSimpleName(), e.getMessage());
+
+            return new LauncherVersion("unknown", 0, null);
+        }
     }
 }
